4f6885678f2b52123458786ccd5f40ba56720f83
doomspec <doomspec@outlook.com>
2022-07-06 17:43:19 -0400

    Clean up old

--- example/circuit_node.py
+++ example/circuit_node.py
@@ -1,4 +1,4 @@
-from mizore.comp_graph.node.mc_node import MetaCircuitNode
+from mizore.comp_graph.node.dc_node import DeviceCircuitNode
 from mizore.transpiler.circuit_runner.circuit_runner import CircuitRunner
 from mizore.comp_graph.comp_graph import CompGraph
 from mizore.operators.observable import Observable
@@ -11,7 +11,7 @@ n_qubit = 2
 mc = MetaCircuit(n_qubit)
 mc.add_gates([SingleRotation(1, 0, 0.5), CNOT(0, 1)])
 obs = Observable(n_qubit, QubitOperator('Z0 Z1'))
-mcnode = MetaCircuitNode(mc, obs, "MyCircuit")
+mcnode = DeviceCircuitNode(mc, obs, "MyCircuit")
 exp_valvar = mcnode()
 
 square_res = exp_valvar*exp_valvar
