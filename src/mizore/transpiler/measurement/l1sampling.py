a5577af16ed6ce713f908d4037233f9d5a5ba67c
doomspec <doomspec@outlook.com>
2022-08-15 18:16:25 -0400

    Update l1 sampling

--- src/mizore/transpiler/measurement/l1sampling.py
+++ src/mizore/transpiler/measurement/l1sampling.py
@@ -1,22 +1,16 @@
-
 from mizore.backend_circuit.backend_op import BackendOperator
 from mizore.backend_circuit.backend_state import BackendState
 from mizore.comp_graph.comp_graph import GraphIterator
 from mizore.comp_graph.node.dc_node import DeviceCircuitNode
-from mizore.comp_graph.node.qc_node import QCircuitNode
 from mizore.operators import QubitOperator
 from mizore.transpiler.circuit_runner.state_processor import StateProcessor
+from mizore.transpiler.measurement.utils import unit_variance
 from mizore.transpiler.transpiler import Transpiler
 from mizore import jax_array
 from math import sqrt
 
-# Var = 4(1-p)p for two-point distribution where probability
-# of being 1 is p and being -1 is (1-p);
-# We set here unit_variance = max Var = 4*0.5*0.5 = 1
-unit_variance = 1.0
 
-
-class NaiveMeasurement(Transpiler):
+class L1Sampling(Transpiler):
 
     def __init__(self, state_ignorant=False, name=None):
         Transpiler.__init__(self, name)
@@ -48,17 +42,15 @@ class NaiveMeasurement(Transpiler):
 
     def get_state_processor(self, node):
         if isinstance(node, DeviceCircuitNode):
-            return NaiveMeasurementStateProcessor(node, state_ignorant=self.state_ignorant)
+            return L1SamplingStateProcessor(node, state_ignorant=self.state_ignorant)
         else:
             return None
 
-
-class NaiveMeasurementStateProcessor(StateProcessor):
+class L1SamplingStateProcessor(StateProcessor):
     def __init__(self, node, state_ignorant=False):
         super().__init__("NaiveMeasurementVarCoeff")
         self._weight_processor = process_state_ignorant if state_ignorant else process_state_aware
         self.obs_list = node.obs_list
-
 
     def process(self, state: BackendState) -> any:
         return self._weight_processor(self.obs_list, state)
@@ -73,51 +65,34 @@ class NaiveMeasurementStateProcessor(StateProcessor):
 def process_state_ignorant(obs_list, state: BackendState):
     var_coeffs = []
     for ob in obs_list:
-        weight_sum = sum([weight for _, weight in ob.terms.items()])
+        weight_sum = sum([abs(weight) for _, weight in ob.terms.items()])
         var_coeffs.append(weight_sum ** 2 * unit_variance)
     return var_coeffs
 
 
-def process_state_aware(obs_list, state: BackendState):
-    var_coeffs = []
-    for ob in obs_list:
-        weight_list = []
-        for qset, op, weight in ob.qset_op_weight():
-            if len(qset) == 0:
-                continue
-            backend_op = BackendOperator(QubitOperator.from_qset_op(qset, op))
-            prob = (backend_op.get_expectation_value(state) + 1) / 2
-            assert prob.imag < 1e-8  # In case the imaginary part is not zero, there must be a bug
-            prob = prob.real
-            var = 4 * (1 - prob) * prob  # variance of two-point distribution
-            if var < 0:
-                var += 1e-14
-            weight_list.append(abs(weight) * sqrt(var))
-        weight_sum = sum(weight_list)
-        var_coeffs.append(weight_sum ** 2)
-    return var_coeffs
-
-
-
-"""
-def get_qc_node_var_coeff(node: QCircuitNode, ob):
-    # TODO here density matrix is not considered. I don't know when to use dm
-    state = node.circuit.get_backend_circuit().get_quantum_state()
-    weight_list = []  # contains a_i*sqrt(Var(<O_i>)) for H=a_iO_i
-    for qset, op, weight in ob.qset_op_weight():
+def get_var_list(ob: QubitOperator, state: BackendState):
+    var_list = []
+    for qset, op, weight in ob.qset_op_weight_omit_const():
         backend_op = BackendOperator(QubitOperator.from_qset_op(qset, op))
-        # TODO check this. Maybe write a test
-        # The probability of getting 1.
-        # (1-prob) is the probability of getting -1
-        # In the state ignorant mode, prob is assumed to be 0.5 to maximize the variance
         prob = (backend_op.get_expectation_value(state) + 1) / 2
         assert prob.imag < 1e-8  # In case the imaginary part is not zero, there must be a bug
         prob = prob.real
         var = 4 * (1 - prob) * prob  # variance of two-point distribution
         if var < 0:
             var += 1e-14
-        weight_list.append(abs(weight) * sqrt(var))
-    del state
-    weight_sum = sum(weight_list)
-    return weight_sum ** 2
-"""
+        # var_list.append(abs(weight) * sqrt(var))
+        var_list.append(var)
+    return var_list
+
+
+def process_state_aware(obs_list, state: BackendState):
+    var_coeffs = []
+    for ob in obs_list:
+        var_list = get_var_list(ob, state)
+        weight_sum = 0.0
+        i = 0
+        for qset, op, weight in ob.qset_op_weight_omit_const():
+            weight_sum += abs(weight) * sqrt(var_list[i])
+            i += 1
+        var_coeffs.append(weight_sum ** 2)
+    return var_coeffs
---
+++ src/mizore/transpiler/measurement/utils.py
@@ -0,0 +1,4 @@
+# Var = 4(1-p)p for two-point distribution where probability
+# of being 1 is p and being -1 is (1-p);
+# We set here unit_variance = max Var = 4*0.5*0.5 = 1
+unit_variance = 1.0
