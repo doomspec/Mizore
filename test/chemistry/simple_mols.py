6415b1478ddcb6800e2b5bd3a1fc87002cceae79
Zijian Zhang <doomspec@outlook.com>
2022-11-12 04:50:55 -0500

    Add Hamiltonians for test

--- test/chemistry/simple_mols.py
+++ test/chemistry/simple_mols.py
@@ -16,20 +16,48 @@ def simple_4_qubit_lih_1():
     hamil.terms = terms
     return hamil
 
+
 def simple_4_qubit_lih():
-    with open(folder_path+"/hamil_cache/lih.pkl", "rb") as f:
+    with open(folder_path + "/hamil_cache/lih.pkl", "rb") as f:
         terms = pickle.load(f)
     hamil = QubitOperator()
     hamil.terms = terms
-    #hamil = hamil * QubitOperator("X0")
+    # hamil = hamil * QubitOperator("X0")
     return hamil - (-7.979466)
 
-def simple_8_qubit_h4():
-    with open(folder_path+"/hamil_cache/h4.pkl", "rb") as f:
+
+def large_12_qubit_lih():
+    with open(folder_path + "/hamil_cache/lih_12.pkl", "rb") as f:
         terms = pickle.load(f)
     hamil = QubitOperator()
     hamil.terms = terms
-    #hamil = hamil * QubitOperator("X0")
+    hamil, _ = hamil.remove_constant()
+    return hamil
+
+
+def read_hamil_by_name(name):
+    with open(folder_path + f"/hamil_cache/{name}.pkl", "rb") as f:
+        terms = pickle.load(f)
+    hamil = QubitOperator()
+    hamil.terms = terms
+    hamil, _ = hamil.remove_constant()
+    return hamil
+
+
+def large_20_qubit_n2():
+    return read_hamil_by_name("n2_20")
+
+
+def large_14_qubit_h2o():
+    return read_hamil_by_name("h2o_14")
+
+
+def simple_8_qubit_h4():
+    with open(folder_path + "/hamil_cache/h4.pkl", "rb") as f:
+        terms = pickle.load(f)
+    hamil = QubitOperator()
+    hamil.terms = terms
+    # hamil = hamil * QubitOperator("X0")
     init_block = Gates(X(0), X(2))
     init_circuit = MetaCircuit(8, blocks=[init_block])
     return hamil - (-1.9961503), init_circuit
