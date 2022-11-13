b36967d11bc6c91d596eac5e2eef5ffd765e67e9
Zijian Zhang <doomspec@outlook.com>
2022-11-12 21:37:12 -0500

    Remove imag part

--- src/mizore/testing/hamil.py
+++ src/mizore/testing/hamil.py
@@ -12,6 +12,9 @@ def get_test_hamil(category, name):
     """
     folder_path = this_folder_path + f"/hamil/{category}/"
     op = QubitOperator.read_op_file(name, folder_path)
+    for pword, coeff in op:
+        op.terms[pword] = float(coeff.real)
+        assert abs(coeff.imag) < 1e-7
     return op
 
 
