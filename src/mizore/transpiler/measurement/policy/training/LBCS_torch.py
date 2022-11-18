44d7516c6c080e470f4bbaca13ca842d4b80b9ec
Zijian Zhang <doomspec@outlook.com>
2022-11-18 14:45:34 -0500

    Add multi-GPU for LBCS training

--- src/mizore/transpiler/measurement/policy/training/LBCS_torch.py
+++ src/mizore/transpiler/measurement/policy/training/LBCS_torch.py
@@ -7,6 +7,11 @@ import numpy as np
 
 from mizore.operators import QubitOperator
 
+
+device = torch.device('cpu')
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+
 torch.manual_seed(3407)  # See https://arxiv.org/abs/2109.08203
 
 pauli_map = {"X": 0, "Y": 1, "Z": 2}
@@ -59,23 +64,36 @@ class LargeLBCS(nn.Module):
         self.heads = torch.nn.Parameter(heads, requires_grad=True)
         self.head_ratios = torch.nn.Parameter(head_ratios, requires_grad=True)
 
-    def forward(self):
+    def forward(self, batch_pauli_tensor, batch_coeff):
         heads = self.activator(self.heads)
         heads = F.normalize(heads, p=1.0, dim=-1)
         head_ratios = self.activator(self.head_ratios)
         head_ratios = F.normalize(head_ratios, p=1.0, dim=0)
-        return heads, head_ratios
+        loss_val = loss(heads, head_ratios, batch_pauli_tensor, batch_coeff)
+        return loss_val
 
 
 def train_model(n_head, hamil, batch_size, n_step=2000):
+    global device 
     n_qubit = hamil.n_qubit
     pauli_tensor, coeffs = get_operator_tensor(hamil, n_qubit)
     pauli_tensor = get_no_zero_pauliwords(pauli_tensor)
+    pauli_tensor = pauli_tensor.to(device)
+    coeffs = coeffs.to(device)
     n_pauliwords = len(coeffs)
-    head_ratios = torch.tensor(np.random.uniform(size=(n_head,), low=5, high=10), dtype=torch.float)
-    heads = torch.tensor(np.random.uniform(size=(n_head, n_qubit, 3), low=5, high=10), dtype=torch.float)
+    head_ratios = torch.tensor(np.random.uniform(size=(n_head,), low=5, high=10), dtype=torch.float).to(device)
+    heads = torch.tensor(np.random.uniform(size=(n_head, n_qubit, 3), low=5, high=10), dtype=torch.float).to(device)
+    model = LargeLBCS(head_ratios, heads).to(device)
 
-    model = LargeLBCS(head_ratios, heads)
+    # https://pytorch.org/docs/stable/generated/torch.nn.parallel.DistributedDataParallel.html#torch.nn.parallel.DistributedDataParallel
+    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    #model = nn.DataParallel(model).to(device)
+
+    #rank = 0
+    #torch.distributed.init_process_group(backend='nccl', rank=rank, world_size=4)
+    #model = torch.nn.parallel.DistributedDataParallel(LargeLBCS(head_ratios, heads), device_ids=[0,1],
+    # output_device=rank).to(device)
+    # model = LargeLBCS(head_ratios, heads).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=1e-5)
 
     n_epoch = 0
@@ -83,9 +101,10 @@ def train_model(n_head, hamil, batch_size, n_step=2000):
     with tqdm(range(n_step), ncols=100) as pbar:
         for step in pbar:
             optimizer.zero_grad()
-            heads, head_ratios = model()
+            # heads, head_ratios = model()
             if n_epoch % 30 == 0:
-                total_loss = loss(heads, head_ratios, pauli_tensor, coeffs)
+                total_loss = torch.sum(model(pauli_tensor, coeffs)).cpu()
+                #print(total_loss)
                 pbar.set_description('Var: {:.6f}'.format(total_loss * (1 - 1 / (2 ** n_qubit + 1))))
             if n_epoch % 5 == 0:
                 randperm = torch.randperm(n_pauliwords)
@@ -94,7 +113,10 @@ def train_model(n_head, hamil, batch_size, n_step=2000):
             batch_pauli_tensor = pauli_tensor[batch_n:batch_n + batch_size]
             batch_coeffs = coeffs[batch_n:batch_n + batch_size]
             batch_n += batch_size
-            loss_val = loss(heads, head_ratios, batch_pauli_tensor, batch_coeffs)
+            #print(batch_coeffs.size())
+            loss_val = model(batch_pauli_tensor, batch_coeffs)
+            loss_val = torch.sum(loss_val)
+            #print(loss_val.device)
             loss_val.backward()
             optimizer.step()
             if batch_n >= n_pauliwords:
@@ -104,10 +126,12 @@ def train_model(n_head, hamil, batch_size, n_step=2000):
 
 if __name__ == '__main__':
     from mizore.testing.hamil import get_test_hamil
-
+    mol_name = "C2H2_24_BK"
+    # mol_name = "LiH_12_BK"
     # jax.config.update('jax_platform_name', 'cuda')
-    n_head = 300
-    hamil, _ = get_test_hamil("mol", "LiH_12_BK").remove_constant()
+    n_head = 3000
+    hamil, _ = get_test_hamil("mol", mol_name).remove_constant()
+    print("Hamiltonian contain {} terms".format(len(hamil.terms)))
     n_qubit = hamil.n_qubit
 
-    train_model(n_head, hamil, 300, n_step=100000)
+    train_model(n_head, hamil, 2200, n_step=100000)
